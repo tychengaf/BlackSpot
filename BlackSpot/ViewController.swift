c3471af6b5fb31f93ba96346e3c3be6e2331938b
Tony Cheng <tychengaf@gmail.com>
2018-09-01 12:29:58 +0800

    Add tap GPS

--- BlackSpot/ViewController.swift
+++ BlackSpot/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate{
 
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateGPSLocation()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +27,27 @@ class ViewController: UIViewController {
     }
 
 
+    func updateGPSLocation(){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            
+            print("longtitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+            
+            let longtitude = String(location.coordinate.longitude)
+            let latitude = String(location.coordinate.latitude)
+
+            //function to compare 
+        }
+    }
 }
 
